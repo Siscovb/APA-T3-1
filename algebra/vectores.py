5b43fb5c146d1df9ec79fa3d80a2137134162870
JoanMarcFuentesSoler <joan.marc.fuentes@estudiantat.upc.edu>
2023-03-24 11:04:30 +0100

    cabeceras_funciones

--- algebra/vectores.py
+++ algebra/vectores.py
@@ -12,51 +12,43 @@ class Vector:
         """
         Constructor de la clase Vector. Su único argumento es un iterable con las componentes del vector.
         """
-
         self.vector = [valor for valor in iterable]
-
         return None      # Orden superflua
 
     def __repr__(self):
         """
         Representación *oficial* del vector que permite construir uno nuevo idéntico mediante corta-y-pega.
         """
-
         return 'Vector(' + repr(self.vector) + ')'
 
     def __str__(self):
         """
         Representación *bonita* del vector.
         """
-
         return str(self.vector)
 
     def __getitem__(self, key):
         """
         Devuelve un elemento o una loncha del vector.
         """
-
         return self.vector[key]
 
     def __setitem__(self, key, value):
         """
         Fija el valor de una componente o loncha del vector.
         """
-
         self.vector[key] = value
 
     def __len__(self):
         """
         Devuelve la longitud del vector.
         """
-
         return len(self.vector)
 
     def __add__(self, other):
         """
         Suma al vector otro vector o una constante.
         """
-
         if isinstance(other, (int, float, complex)):
             return Vector(uno + other for uno in self)
         else:
@@ -68,20 +60,58 @@ class Vector:
         """
         Invierte el signo del vector.
         """
-
         return Vector([-1 * item for item in self])
 
     def __sub__(self, other):
         """
         Resta al vector otro vector o una constante.
         """
-
         return -(-self + other)
 
     def __rsub__(self, other):     # No puede ser __rsub__ = __sub__
         """
         Método reflejado de la resta, usado cuando el primer elemento no pertenece a la clase Vector.
         """
-
         return -self + other
+
+    def __mul__(self, other):
+        """
+        Función para multiplicar un vector con otro vector o numero
+        """
+        ...
+    
+    __rmul__ = __mul__ # Se puede hacer esta igualación porque la multiplicacion es comutativa 2x1 = 1x2
+    
+    def __matmul__(self,other):
+        """
+        Función para multiplicar un vector con otro vector o numero
+        """
+        ...
+
+    def __floordiv__(self,other):
+        """
+        Función para calcular la componente tangencial(paralela) de dos vectores
+        """
+        ...
+
+    def __rfloordiv__(self,other):
+        """
+        Función para calcular la componente tangencial(paralela) de dos vectores
+        """
+        ...
+    
+    def __mod__(self, other):
+        """
+        Función para calcular la componente normal(perpendicular) de dos vectores
+        """
+        ...
+    
+    def __rmod__(self, other):
+        """
+        Función para calcular la componente normal(perpendicular) de dos vectores
+        """
+        ...
+    
+import doctest
+doctest.testmod()
 
